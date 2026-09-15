aeeab0e81e4a14a3b3603fec01c312992652a7f9
Yann Santana Bitencourt <yann.bitencourt@ba.estudante.senai.br>
2026-09-15 16:01:11 -0300

    Adiciona a função de multiplicação

--- app/calculadora.py
+++ app/calculadora.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def somar(a, b):
     return a + b
 
@@ -13,3 +14,14 @@ def calcular_desconto(preco, percentual):
 
 def multiplicar(a, b):
     return a * b
+=======
+def somar(a,b):
+    return a+b
+
+def subtrair(a,b):
+    return a-b
+
+def calcular_desconto(preco,percentual):
+    desconto = preco * percentual / 100
+    return preco - desconto
+>>>>>>> 42f727fab38e52a49b25f5b1412f890954e08f30
--- tests/test_calculadora.py
+++ tests/test_calculadora.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from app.calculadora import (
     somar,
     subtrair,
@@ -24,3 +25,22 @@ def test_calcular_desconto():
 def test_multiplicar():
     resultado = multiplicar(4, 5)
     assert resultado == 20
+=======
+from app.calculadora import(
+    somar,
+    subtrair,
+    calcular_desconto
+)
+
+def test_somar():
+    resultado = somar(2,3)
+    assert resultado == 5
+    
+def test_subtrair():
+    resultado = subtrair(10,4)
+    assert resultado == 6
+        
+def test_calcular_desconto():
+    resultado = calcular_desconto(100,10)
+    assert resultado == 90
+>>>>>>> 42f727fab38e52a49b25f5b1412f890954e08f30
